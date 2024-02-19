93ae98a7b7bc96a06e3d2c3cb0b633432329e8ba
Mendel Cyprys <mmcyprys@gmail.com>
2024-02-19 14:20:23 +0000

    Initial commit
    Files uploaded

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, g
+import sqlite3
+from scripts.schema import initialise
+
+app = Flask(__name__)
+
+DATABASE = 'db/database.sqlite3'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+with app.app_context():
+    cur = get_db().cursor()
+    initialise(cur)
+
+@app.route("/")
+def home():
+    cur = get_db().cursor()
+    return render_template('home.html', name='Home')
+
+@app.route("/doctors")
+def doctors():
+    cur = get_db().cursor()
+    return render_template('doctors.html', name='Doctors')
+
+@app.route("/patients")
+def patients():
+    return render_template(
+        'patients.html',
+        patient_data=query_db('select * from patients'),
+    )
+
+@app.route("/appointments")
+def appointments():
+    cur = get_db().cursor()
+    return render_template('appointments.html', name='Appointments')
